4845323a82c151775926f277114827c5adc48242
Nelson-77 <nelsonselvaraj62@gmail.com>
2025-02-02 16:29:02 +0530

    Create Calculate the Power of a Number.py

---
+++ 02.02.2025/Calculate the Power of a Number.py
@@ -0,0 +1,7 @@
+def power(base, exponent):
+    return base ** exponent
+
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+result = power(base, exponent)
+print("The result is:", result)
